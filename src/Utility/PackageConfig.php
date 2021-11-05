b81decb4d07e7e3389072e699810b49d8be64d6d
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-11-05 15:50:47 +0200

    update pachage config

--- src/Utility/PackageConfig.php
+++ src/Utility/PackageConfig.php
@@ -6,7 +6,34 @@ namespace ByTIC\PackageBase\Utility;
  * Class PackageConfig
  * @package ByTIC\PackageBase\Utility
  */
-class PackageConfig
+abstract class PackageConfig
 {
+    protected static $cache = [];
+    public const NAME = null;
 
+    /**
+     * @param string $type
+     * @param null|string $default
+     * @return string
+     */
+    public static function value(string $type, $default = null)
+    {
+        if (isset(static::$cache[$type])) {
+            return static::$cache[$type];
+        }
+
+        if (!function_exists('config')) {
+            return $default;
+        }
+
+        $varName = self::NAME . '.' . $type;
+        $config = config();
+        if ($config->has($varName) === false) {
+            return $default;
+        }
+
+        $value = $config->get($varName);
+        static::$cache[$type] = $value;
+        return $value;
+    }
 }
